63f7b0413947dda2138859f9913c37ddf7132040
z0nkee191 <yaroslavdudar6@gmail.com>
2025-08-22 17:54:37 +0300

    all h/w

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
+
+// const top = document.querySelector("#top")
+// top.addEventListener("click", function () {
+//   window.scrollTo(0, 0);
+// });
